08569632e9c4c481ed78803f4c24823c8aada463
Khanh Tran Cong <khanh.trancong@pycogroup.com>
2019-08-21 18:47:11 +0700

    Init project

---
+++ index.js
@@ -0,0 +1,15 @@
+import App from './App';
+
+import { Navigation } from "react-native-navigation";
+
+Navigation.registerComponent(`navigation.playground.WelcomeScreen`, () => App);
+
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      component: {
+        name: "navigation.playground.WelcomeScreen"
+      }
+    }
+  });
+});
